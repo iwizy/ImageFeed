6f8b14d4da74fadf4cb5f47c1b5ef9c3688a0f79
Alex Agafonov <agafonov.alexander@gmail.com>
2025-04-08 23:43:11 +0300

    Верстка экрана профиля кодом

--- ImageFeed/ImagesList/ProfileViewController.swift
+++ ImageFeed/ImagesList/ProfileViewController.swift
@@ -2,21 +2,94 @@
 //  ProfileViewController.swift
 //  ImageFeed
 //
-//  Класс вью контролдлера профиля
+//  Класс вью контроллера профиля
 
 import UIKit
 
-class ProfileViewController: UIViewController {
-
-    @IBOutlet weak var avatarImage: UIImageView!
+final class ProfileViewController: UIViewController {
     
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var loginNameLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
+    // Делаем опциональную переменную для вью
+    var profileImageView: UIImageView?
     
-    @IBOutlet weak var exitButton: UIButton!
+    // Функция установки аватарки профиля
+    private func profileImageSetup() {
+        let profileImage = UIImage(named: "profile_photo")
+        let profileImageView = UIImageView(image: profileImage)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(profileImageView)
+        
+        profileImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        profileImageView.layer.cornerRadius = 35
+        profileImageView.layer.masksToBounds = true
+        
+        profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
+        profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        self.profileImageView = profileImageView
+        
+    }
     
-    @IBOutlet weak var avatarClicked: UIImageView!
-    @IBOutlet weak var logoutButtonClicked: UIButton!
+    // Функция кнопки выхода
+    private func logoutButtonSetup() {
+        let logoutImage = UIImage(named: "logout_button")
+        guard let logoutImage else { return }
+        let logoutButton = UIButton.systemButton(with: logoutImage, target: self, action: nil)
+        logoutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logoutButton)
+        
+        logoutButton.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        logoutButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+        guard let profileImageView else { return }
+        logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor).isActive = true
+        logoutButton.tintColor = UIColor(named: "YP Red")
+        
+    }
+    
+    // Функция размещения лейблов
+    private func labelsSetup() {
+        let nameLabel = UILabel()
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(nameLabel)
+        
+        nameLabel.text = "Александр Агафонов"
+        nameLabel.textColor = UIColor(named: "YP White")
+        nameLabel.font = UIFont(name: "SF Pro", size: 23)
+        nameLabel.font = UIFont.boldSystemFont(ofSize: 23)
+        nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        guard let profileImageView else { return }
+        nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8).isActive = true
+        
+        
+        
+        let loginNameLabel = UILabel()
+        loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(loginNameLabel)
+        
+        loginNameLabel.text = "@iWizard"
+        loginNameLabel.textColor = UIColor(named: "YP Gray")
+        loginNameLabel.font = UIFont(name: "SF Pro", size: 13)
+        loginNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        let descriptionLabel = UILabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(descriptionLabel)
+        
+        descriptionLabel.text = "Hello, reviewer!"
+        descriptionLabel.textColor = UIColor(named: "YP White")
+        descriptionLabel.font = UIFont(name: "SF Pro", size: 13)
+        descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8).isActive = true
+        
+        
+    }
+    
+    override func viewDidLoad() {
+        profileImageSetup()
+        logoutButtonSetup()
+        labelsSetup()
+    }
+    
+    
 }
-
