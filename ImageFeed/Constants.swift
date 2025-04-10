1dc26d6e48d1fd00a2e8160f4d89d6e012b5943b
Alex Agafonov <agafonov.alexander@gmail.com>
2025-04-10 23:57:53 +0300

    Добавлены константы в отдельном файле

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Константы приложения
+
+import Foundation
+
+enum Constants {
+    static let accessKey: String = "RYDyjUfjvCaitGIKzQckqTyHtbQstQraAfl0WE0nTF8"
+    static let secretKey: String = "-eN7f8mEWdpjsUedu-_kmSxzxGw-DN3W6NUaBSagTSE"
+    static let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope: String = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
