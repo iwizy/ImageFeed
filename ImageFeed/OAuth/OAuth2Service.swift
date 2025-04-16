3f49126ad4c5d54a4746aa98caea36ee2d7b2cab
Alex Agafonov <agafonov.alexander@gmail.com>
2025-04-16 23:30:54 +0300

    Создание класса OAuth2Service

---
+++ ImageFeed/OAuth/OAuth2Service.swift
@@ -0,0 +1,11 @@
+//
+//  OAuth2Service.swift
+//  ImageFeed
+//
+//  Класс сервиса авторизации
+
+final class OAuth2Service {
+    static let shared = OAuth2Service()
+    private init() {}
+    
+}
