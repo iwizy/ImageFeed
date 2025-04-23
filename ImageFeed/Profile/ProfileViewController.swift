3b2dceae14e549b1b1e6abc83362873f76d6c4e1
Alex Agafonov <agafonov.alexander@gmail.com>
2025-04-23 19:06:20 +0300

    Правка стиля верстки профиля

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -23,12 +23,14 @@ final class ProfileViewController: UIViewController {
         view.addSubview(profileImageView)
         
         // Констрейнты
-        profileImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
-        profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
         profileImageView.layer.cornerRadius = 35
         profileImageView.layer.masksToBounds = true
-        profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32).isActive = true
-        profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        NSLayoutConstraint.activate([
+            profileImageView.widthAnchor.constraint(equalToConstant: 70),
+            profileImageView.heightAnchor.constraint(equalToConstant: 70),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            profileImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16)
+        ])
         // Сохраняем ссылку на imageView в свойство класса
         self.profileImageView = profileImageView
     }
@@ -41,14 +43,14 @@ final class ProfileViewController: UIViewController {
         logoutButton.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(logoutButton)
         
-        // Установка размеров кнопки
-        logoutButton.widthAnchor.constraint(equalToConstant: 44).isActive = true
-        logoutButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
-        
-        // Констрейнты
-        logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
         guard let profileImageView else { return } // Проверяем, что profileImageView был создан
-        logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor).isActive = true
+        // Установка размеров кнопки
+        NSLayoutConstraint.activate([
+            logoutButton.widthAnchor.constraint(equalToConstant: 44),
+            logoutButton.heightAnchor.constraint(equalToConstant: 44),
+            logoutButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor)
+        ])
         logoutButton.tintColor = UIColor(named: "YP Red")
     }
     
@@ -63,12 +65,9 @@ final class ProfileViewController: UIViewController {
         nameLabel.text = "Александр Агафонов"
         nameLabel.textColor = UIColor(named: "YP White")
         nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
-
-        // Констрейнты
-        nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        
         guard let profileImageView else { return }
-        nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8).isActive = true
-    
+        
         // MARK: Login Label
         let loginNameLabel = UILabel()
         loginNameLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -78,10 +77,6 @@ final class ProfileViewController: UIViewController {
         loginNameLabel.text = "@iWizard"
         loginNameLabel.textColor = UIColor(named: "YP Gray")
         loginNameLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
-        
-        // Констрейнты
-        loginNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
         
         // MARK: Description Label
         let descriptionLabel = UILabel()
@@ -94,8 +89,15 @@ final class ProfileViewController: UIViewController {
         descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
         
         // Констрейнты
-        descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-        descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8).isActive = true
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            loginNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            loginNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            descriptionLabel.topAnchor.constraint(equalTo: loginNameLabel.bottomAnchor, constant: 8)
+        ])
+        
     }
     
     // MARK: Lifecycle
