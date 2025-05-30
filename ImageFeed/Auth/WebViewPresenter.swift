e383bc2696b03c0afd120d371e2b718540e23c83
Alex Agafonov <agafonov.alexander@gmail.com>
2025-05-31 00:29:33 +0300

    Создание Auth-хелпера

--- ImageFeed/Auth/WebViewPresenter.swift
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -7,12 +7,6 @@
 import Foundation
 import WebKit
 
-
-enum WebViewConstants {
-    // Базовый URL для авторизации через Unsplash OAuth
-    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
-}
-
 public protocol WebViewPresenterProtocol {
     var view: WebViewViewControllerProtocol? { get set }
     func viewDidLoad()
@@ -21,26 +15,20 @@ public protocol WebViewPresenterProtocol {
 }
 
 final class WebViewPresenter: WebViewPresenterProtocol {
-    func code(from url: URL) -> String? {
-        // Проверяем все необходимые условия для извлечения кода:
-        guard
-            let urlComponents = URLComponents(string: url.absoluteString), // Должны быть компоненты URL
-            urlComponents.path == "/oauth/authorize/native", // Путь должен соответствовать OAuth callback
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" }) // Должен присутствовать query-параметр "code"
-        else {
-            return nil
-        }
-        
-        return codeItem.value
-    }
     
     weak var view: WebViewViewControllerProtocol?
     
+    var authHelper: AuthHelperProtocol
+    
+    init(authHelper: AuthHelperProtocol) {
+            self.authHelper = authHelper
+        }
     
     func viewDidLoad() {
+        
+        guard let request = authHelper.authRequest() else { return }
         // Создаем компоненты URL из базового адреса авторизации
-        guard var urlComponents = URLComponents(string: WebViewConstants.unsplashAuthorizeURLString) else {
+        guard var urlComponents = URLComponents(string: Constants.unsplashAuthorizeURLString) else {
             print("Ошибка: Не удалось создать URL компоненты")
             return
         }
@@ -60,9 +48,13 @@ final class WebViewPresenter: WebViewPresenterProtocol {
         }
         
         // Создаем и выполняем запрос
-        let request = URLRequest(url: url)
+        
         view?.load(request: request)
         didUpdateProgressValue(0)
+    }
+    
+    func code(from url: URL) -> String? {
+        authHelper.code(from: url)
     }
     
     func didUpdateProgressValue(_ newValue: Double) {
@@ -76,5 +68,7 @@ final class WebViewPresenter: WebViewPresenterProtocol {
     func shouldHideProgress(for value: Float) -> Bool {
         abs(value - 1.0) <= 0.0001
     }
+    
+    
     
 }
