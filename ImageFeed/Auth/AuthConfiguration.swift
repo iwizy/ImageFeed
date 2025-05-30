750cf1898c0a4e56446557439c7495c4eda15984
Alex Agafonov <agafonov.alexander@gmail.com>
2025-05-31 00:31:03 +0300

    Обновленный файл констант

---
+++ ImageFeed/Auth/AuthConfiguration.swift
@@ -0,0 +1,45 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Константы приложения
+
+import Foundation
+
+enum Constants {
+    static let accessKey: String = "RYDyjUfjvCaitGIKzQckqTyHtbQstQraAfl0WE0nTF8"
+    static let secretKey: String = "-eN7f8mEWdpjsUedu-_kmSxzxGw-DN3W6NUaBSagTSE"
+    static let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope: String = "public+read_user+write_likes"
+    
+    static let defaultBaseURL: URL? = URL(string: "https://api.unsplash.com")
+    static let unsplashAuthorizeURLString = "https://unsplash.com/oauth/authorize"
+    
+}
+
+struct AuthConfiguration {
+    let accessKey: String
+    let secretKey: String
+    let redirectURI: String
+    let accessScope: String
+    let defaultBaseURL: URL?
+    let authURLString: String
+
+    init(accessKey: String, secretKey: String, redirectURI: String, accessScope: String, authURLString: String, defaultBaseURL: URL?) {
+        self.accessKey = accessKey
+        self.secretKey = secretKey
+        self.redirectURI = redirectURI
+        self.accessScope = accessScope
+        self.defaultBaseURL = defaultBaseURL
+        self.authURLString = authURLString
+    }
+    
+    static var standard: AuthConfiguration {
+            return AuthConfiguration(accessKey: Constants.accessKey,
+                                     secretKey: Constants.secretKey,
+                                     redirectURI: Constants.redirectURI,
+                                     accessScope: Constants.accessScope,
+                                     authURLString: Constants.unsplashAuthorizeURLString,
+                                     defaultBaseURL: Constants.defaultBaseURL)
+        }
+}
